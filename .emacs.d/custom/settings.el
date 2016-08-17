01f5a3179133622417bb4527beed2c46d6fcec61
xevgeny <e.tskhovrebov@pm.me>
2016-08-17 14:44:39 +0200

    Add paredit kbd's

--- .emacs.d/custom/settings.el
+++ .emacs.d/custom/settings.el
@@ -8,16 +8,18 @@
 ;; Helm settings
 (helm-mode 1)
 (global-set-key (kbd "M-x") 'helm-M-x)
+(setq helm-split-window-in-side-p t) ;; helm split window
 
-;; IDO-mode
-(ido-mode t)
+(projectile-global-mode) ;; enable projectile mode
+(ido-mode t)             ;; interactively-do mode
+(show-paren-mode)        ;; highlight parenthesis
 
-;; Projectile
-(projectile-global-mode)
+;; Emacs
+(global-set-key (kbd "C-x f") 'find-file) ;; find file
 
 ;; Helm
-(global-set-key (kbd "C-c o") 'helm-projectile-find-file)
-(global-set-key (kbd "C-c f") 'helm-do-ag-project-root)
+(global-set-key (kbd "C-c o") 'helm-projectile-find-file) ;; find file in project
+(global-set-key (kbd "C-c f") 'helm-do-ag-project-root)   ;; find in files
 (global-set-key (kbd "C-c s") 'helm-projectile-switch-project)
 (global-set-key (kbd "C-c b") 'helm-buffers-list)
 (global-set-key (kbd "C-c r") 'helm-recentf)
@@ -30,13 +32,34 @@
 (global-set-key (kbd "C-c g r") 'vc-revert-buffer)
 (global-set-key (kbd "C-c g a") 'vc-annotate)
 (global-set-key (kbd "C-c g d") 'magit-diff)
+(global-set-key (kbd "C-c g b") 'magit-blame)
+
+;; paredit
+(global-set-key (kbd "<A-M-right>") 'paredit-forward-slurp-sexp)
+(global-set-key (kbd "<A-M-left>")  'paredit-backward-slurp-sexp)
+
+(global-set-key (kbd "<A-M-down>") 'paredit-forward-barf-sexp)
+(global-set-key (kbd "<A-M-up>")   'paredit-backward-barf-sexp)
+
+(global-set-key (kbd "C-c p f") 'paredit-splice-sexp-killing-forward)
+(global-set-key (kbd "C-c p b") 'paredit-splice-sexp-killing-backward)
+
+(global-set-key (kbd "C-c w (") 'paredit-wrap-round)
+(global-set-key (kbd "C-c w <") 'paredit-wrap-angled)
+(global-set-key (kbd "C-c w [") 'paredit-wrap-square)
+(global-set-key (kbd "C-c w {") 'paredit-wrap-curly)
+
+(global-set-key (kbd "C-c (") 'paredit-open-round)
+(global-set-key (kbd "C-c <") 'paredit-open-angled)
+(global-set-key (kbd "C-c {") 'paredit-open-square)
+(global-set-key (kbd "C-c {") 'paredit-open-curly)
+
+(global-set-key (kbd "C-c )") 'paredit-close-round)
+(global-set-key (kbd "C-c >") 'paredit-close-angled)
+(global-set-key (kbd "C-c ]") 'paredit-close-square)
+(global-set-key (kbd "C-c }") 'paredit-close-curly)
 
 ;; Neotree
-(global-set-key [f8] 'neotree-toggle)
+(global-set-key (kbd "C-c e") 'neotree-toggle)
 ;; Every time when the neotree window is opened, let it find current file and jump to node.
 (setq neo-smart-open t)
-;; When running ‘projectile-switch-project’ ‘neotree’ will change root automatically
-;; (setq projectile-switch-project-action 'neotree-projectile-action)
-
-;; Helm split window
-(setq helm-split-window-in-side-p t)
