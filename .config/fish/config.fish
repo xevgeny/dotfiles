802bc829e4ddafaa920714389ded07dbef26fd18
Evgeny Tskhovrebov <e.tskhovrebov@pm.me>
2024-08-03 12:34:48 +0200

    Update config.fish
    Fix dot alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 set -x LC_ALL en_US.UTF-8
 set -x TERM xterm-256color
 
-alias dotfiles='git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
+alias dot='git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
 
 # disable timestamp on the right
 function fish_right_prompt
